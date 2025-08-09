5e07d59b98441e934f7b076438ec4520744a6c87
ameeenamzz <shoukethameen@gmail.com>
2025-08-09 10:15:25 +0530

    refactored userCard

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -10,13 +10,16 @@ const UserCard = ({ user }) => {
           <img src={photURL} alt="Shoes" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{firstName}</h2>
+          <h2 className="card-title">
+            {firstName} {lastName}
+          </h2>
           <p>
             A card component has a figure, a body part, and inside body there
             are title and actions parts
           </p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+          <div className="card-actions flex justify-center mt-5 ">
+            <button className="btn btn-primary">Follow</button>
+            <button className="btn btn-secondary">Ignore</button>
           </div>
         </div>
       </div>
